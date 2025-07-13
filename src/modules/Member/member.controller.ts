88df1775890caed19e8a9c219f96a26b5a1859dd
Mike <mikemike@Mikeui-MacBookPro.local>
2025-07-13 14:53:36 +0900

    feat:develop

--- src/modules/Member/member.controller.ts
+++ src/modules/Member/member.controller.ts
@@ -22,7 +22,9 @@ import {
   NAVER_TOKEN_URL,
   NAVER_USER_INFO_URL,
 } from "../../libs/utils/constants";
-import * as jose from "jose";
+// @ts-ignore
+const jose = require("jose");
+
 import {
   JWT_EXPIRATION_TIME,
   JWT_SECRET,
