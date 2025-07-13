90da86eb2b040a79b38e53835b003e5c9337e067
Mike <mikemike@Mikeui-MacBookPro.local>
2025-07-13 14:36:35 +0900

    feat: develop

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,7 @@ echo "Building the application..."
 npm run build
 
 echo "Starting the application in production mode using PM2..."
-pm2 start dist/server.js --name=car-washing-service --env production
+pm2 start dist/server.js --name=fastify-backend-project --env production
 
-# Start ngrok with PM2 on the desired port (change 3110 to your Fastify port)
-echo "Starting ngrok tunnel using PM2..."
-pm2 start "ngrok http 3112 --log=stdout" --name=ngrok-tunnel
 
 echo "Production deployment complete!"
